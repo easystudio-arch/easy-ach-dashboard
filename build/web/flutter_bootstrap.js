7bd127cee473f69f6dfd45edb5be9d9ba2155e4a
m_soleh <mohamad.soleh@nabatisnack.co.id>
2026-06-12 08:27:03 +0700

    deploy web

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2356608091" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3315346559" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
